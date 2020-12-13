9bfacf646c1ccb63ce09b69344bfc54a8c009907
S7rizh <antonov9312@yandex.ru>
2020-12-13 12:21:39 +0300

    Add operation on simple timer

--- app/src/main/java/com/example/innotime/addTimer/AddSimpleTimer.kt
+++ app/src/main/java/com/example/innotime/addTimer/AddSimpleTimer.kt
@@ -7,10 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.example.innotime.R
+import com.example.innotime.SequentialSingleTimerInfo
+import com.example.innotime.SequentialTimerInfo
 import com.example.innotime.db.TimerDbModel
 import com.example.innotime.db.TimerRoomDatabase
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_add_simple_timer.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.NumberFormatException
+import java.util.*
+import kotlin.coroutines.EmptyCoroutineContext
 
 class AddSimpleTimer : Fragment() {
     override fun onCreateView(
@@ -28,17 +36,46 @@ class AddSimpleTimer : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         back.setOnClickListener { goToMainPage() }
 
-//        GET NAME && DESCRIPTION
-//        mSettings?.getString(APP_PREFERENCES_NAME, "")
-//        mSettings?.getString(APP_PREFERENCES_DESCRIPTION, "")
         create.setOnClickListener {
 //            Toast.makeText(activity, "Need to save somewhere", Toast.LENGTH_LONG).show()
-            val timersDao = TimerRoomDatabase.getTimerDataBase(activity!!.application).timerDao()
 
-            val gson = Gson()
-//            gson.toJson()
+//            GET NAME && DESCRIPTION
+//            mSettings?.getString(APP_PREFERENCES_NAME, "")
+//            mSettings?.getString(APP_PREFERENCES_DESCRIPTION, "")
 
-//            timersDao.insertTimer(TimerDbModel(0, ))
+            try {
+
+                val timersDao = TimerRoomDatabase.getTimerDataBase(activity!!.application).timerDao()
+
+                val uuid = UUID.randomUUID().toString()
+                val duration = time.text.toString().toLong()
+
+                val gson = Gson()
+                val newDbModel =
+                    TimerDbModel(
+                        0,
+                        gson.toJson(
+                            SequentialTimerInfo(
+                                uuid,
+                                mSettings?.getString(APP_PREFERENCES_NAME, "") ?: uuid,
+                                mSettings?.getString(APP_PREFERENCES_DESCRIPTION, "") ?: "",
+                                arrayOf(SequentialSingleTimerInfo(0, 10, "", "", emptyArray())),
+                                0
+                            )
+                        ).toString()
+                    )
+
+
+                CoroutineScope(EmptyCoroutineContext).launch(Dispatchers.IO) {
+                    timersDao.insertTimer(newDbModel)
+                    activity?.finish()
+                }
+
+            } catch (ex: NumberFormatException) {
+                Toast.makeText(this.activity, "Incorrect duration!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
 
         }
     }
